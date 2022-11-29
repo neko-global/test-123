c550d9ddb6aa2b728cc3ed6bc1d2f4bab8cab5df
Mac <longhd240195@gmail.com>
2022-11-29 15:28:47 +0700

    udpate code

---
+++ Assets/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public void Testing(){
+        Debug.Log("testing");
+    }
+}
